8545dd5cb7fdecc0fd9af25acec7de66206ae8cd
abvbdelrahman <98822056+abvbdelrahman@users.noreply.github.com>
2025-11-04 19:21:49 +0200

    Update Subscription.js

--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -4,7 +4,7 @@ const subscriptionSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: 'Coach',
       required: true,
     },
     plan: {
